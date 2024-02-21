5dcfba4d14305e533fcba3634864a42397fb0cc4
ajchapman777 <ajchapman777@gmail.com>
2024-02-21 12:35:13 -0800

    7-model_state_fetch_all.

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,11 +1,3 @@
-#!/usr/bin/python3
-"""Start link class to table in database
-"""
-import sys
-from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import (create_engine)
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+Base = declarative_base()
